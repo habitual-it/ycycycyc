544c3b125375cc368c1574399674afd77510d250
习惯 <918897554@qq.com>
2025-12-01 01:55:42 +0800

    jhg

--- android-agent/settings.gradle.kts
+++ android-agent/settings.gradle.kts
@@ -1,5 +1,6 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -9,6 +10,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
         google()
         mavenCentral()
     }
